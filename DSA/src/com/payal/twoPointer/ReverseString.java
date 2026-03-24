9e0577e27e896b6e1766dc6ba4447fa92a60b5d9
Payal Desai <p.desai0108@gmail.com>
2026-03-23 21:13:46 -0400

    reverse string

--- DSA/src/com/payal/twoPointer/ReverseString.java
+++ DSA/src/com/payal/twoPointer/ReverseString.java
@@ -3,8 +3,27 @@ package com.payal.twoPointer;
 public class ReverseString {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String str = "Payal";
+		
+		String result = reverseString(str);
+		
+		System.out.println("Result : " + result);
+	}
 
+	private static String reverseString(String str) {
+		char[] arr = str.toCharArray();
+		int left = 0;
+		int right = str.length() - 1;
+		
+		while(left < right) {
+			char temp = arr[left];
+			arr[left] = arr[right];
+			arr[right] = temp;
+			left++;
+			right--;
+			
+		}
+		return new String(arr);
 	}
 
 }
